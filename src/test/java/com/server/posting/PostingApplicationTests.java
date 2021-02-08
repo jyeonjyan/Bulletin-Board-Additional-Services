d67d1acd0129f800cffc7b36a02c685ae0e0fcbd
johnjihwan <s20062@gsm.hs.kr>
2021-02-08 17:41:14 +0900

    [UPDATE]Basic File Structure

---
+++ src/test/java/com/server/posting/PostingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.server.posting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
